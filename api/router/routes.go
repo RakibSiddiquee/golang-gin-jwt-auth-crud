a0ed4dc975a8c454dbc42d3cd27db4b0d01d680c
RakibSiddiquee <rakibsiddiquee@gmail.com>
2023-08-21 00:08:14 +0600

    Route file updated

--- api/router/routes.go
+++ api/router/routes.go
@@ -1,54 +1,56 @@
 package router
 
 import (
-	controllers2 "github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/api/controllers"
+	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/api/controllers"
 	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/api/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func GetRoute(r *gin.Engine) {
 	// User routes
-	r.POST("/api/signup", controllers2.Signup)
-	r.POST("/api/login", controllers2.Login)
+	r.POST("/api/signup", controllers.Signup)
+	r.POST("/api/login", controllers.Login)
 
 	r.Use(middleware.RequireAuth)
-	r.POST("/api/logout", controllers2.Logout)
+	r.POST("/api/logout", controllers.Logout)
+	userRouter := r.Group("/api/users")
+	{
+		userRouter.GET("/", controllers.GetUsers)
+	}
 
 	// Category routes
 	catRouter := r.Group("/api/categories")
 	{
-		catRouter.Use(middleware.RequireAuth)
+		//catRouter.Use(middleware.RequireAuth)
 
-		catRouter.GET("/", controllers2.GetCategories)
-		catRouter.POST("/create", controllers2.CreateCategory)
-		catRouter.GET("/:id/edit", controllers2.EditCategory)
-		catRouter.PUT("/:id/update", controllers2.UpdateCategory)
-		catRouter.DELETE("/:id/delete", controllers2.DeleteCategory)
-		catRouter.GET("/all-trash", controllers2.GetTrashCategories)
-		catRouter.DELETE("/delete-permanent/:id", controllers2.DeleteCategoryPermanent)
+		catRouter.GET("/", controllers.GetCategories)
+		catRouter.POST("/create", controllers.CreateCategory)
+		catRouter.GET("/:id/edit", controllers.EditCategory)
+		catRouter.PUT("/:id/update", controllers.UpdateCategory)
+		catRouter.DELETE("/:id/delete", controllers.DeleteCategory)
+		catRouter.GET("/all-trash", controllers.GetTrashCategories)
+		catRouter.DELETE("/delete-permanent/:id", controllers.DeleteCategoryPermanent)
 	}
 
 	// Post routes
 	postRouter := r.Group("/api/posts")
 	{
-		postRouter.Use(middleware.RequireAuth)
-		postRouter.GET("/", controllers2.GetPosts)
-		postRouter.POST("/create", controllers2.CreatePost)
-		postRouter.GET("/:id/show", controllers2.ShowPost)
-		postRouter.GET(":id/edit", controllers2.EditPost)
-		postRouter.PUT("/:id/update", controllers2.UpdatePost)
-		postRouter.DELETE("/:id/delete", controllers2.DeletePost)
-		postRouter.GET("/all-trash", controllers2.GetTrashedPosts)
-		postRouter.DELETE("/delete-permanent/:id", controllers2.PermanentlyDeletePost)
+		postRouter.GET("/", controllers.GetPosts)
+		postRouter.POST("/create", controllers.CreatePost)
+		postRouter.GET("/:id/show", controllers.ShowPost)
+		postRouter.GET(":id/edit", controllers.EditPost)
+		postRouter.PUT("/:id/update", controllers.UpdatePost)
+		postRouter.DELETE("/:id/delete", controllers.DeletePost)
+		postRouter.GET("/all-trash", controllers.GetTrashedPosts)
+		postRouter.DELETE("/delete-permanent/:id", controllers.PermanentlyDeletePost)
 	}
 
 	// Comment routes
 	commentRouter := r.Group("/api/posts/:id/comment")
 	{
-		commentRouter.Use(middleware.RequireAuth)
-		commentRouter.POST("/store", controllers2.CommentOnPost)
-		commentRouter.GET("/:comment_id/edit", controllers2.EditComment)
-		commentRouter.PUT("/:comment_id/update", controllers2.UpdateComment)
-		commentRouter.DELETE("/:comment_id/delete", controllers2.DeleteComment)
+		commentRouter.POST("/store", controllers.CommentOnPost)
+		commentRouter.GET("/:comment_id/edit", controllers.EditComment)
+		commentRouter.PUT("/:comment_id/update", controllers.UpdateComment)
+		commentRouter.DELETE("/:comment_id/delete", controllers.DeleteComment)
 	}
 }
